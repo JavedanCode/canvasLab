bfcd57333ced1cd2d748cca43222ee80c99c42e4
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-04-30 16:30:31 +0300

    refactor userController to use userServices for separation of concerns

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,62 +1,66 @@
 const bcrypt = require("bcrypt");
 const db = require("../config/db"); //import database
 const jwt = require("jsonwebtoken");
+const { getAllUsers, getUserById, createUser, removeUser } = require("../services/userService");
 
-const getUsers = (req, res, next) => {
-  db.query("SELECT * FROM users", (err, result) => {
-    if (err) {
-      return res.status(500).json({ message: "Internal server error" });
-    }
-
-    return res.status(200).json({ data: result });
-  });
-};
-
-const getUser = (req, res, next) => {
-  db.query(
-    `SELECT * FROM users WHERE id = $1`,
-    [req.params.id],
-    (err, result) => {
-      if (err) {
-        return res.status(500).json({ message: "Internal server error" });
-      }
-
-      if (result.length === 0)
-        return res.status(404).json({ message: "User not found" });
-      return res.status(200).json({ data: result[0] });
-    },
-  );
-};
-
-const registerUser = async (req, res, next) => {
-  if (!req.body.username || !req.body.email || !req.body.password) {
-    return res.status(400).json({ error: "All fields are required" });
-  }
-
-  try {
-    const { username, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    db.query(
-      `INSERT INTO users(username, email, password_hash) 
-    VALUES($1,$2,$3)`,
-      [username, email, hashedPassword],
-      (err, result) => {
-        if (err) {
-          if (err.code === `ER_DUP_ENTRY`) {
-            return res.status(409).json({ error: "User already exists" });
-          }
-          return res.status(500).json({ message: "Internal server error" });
-        }
-        const userId = result.insertId;
-        return res
-          .status(201)
-          .json({ message: "User created successfully", userId: userId });
-      },
-    );
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
+// GET ALL USERS
+const getUsers = async (req, res) => {
+  try{
+    const users = await getAllUsers();
+    return res.status(200).json({data: users});
+  }catch{
+    return res.status(500).json({message: "Internal server error"});
   }
 };
+
+// GET USER BY ID
+const getUser = async (req, res) => {
+   try{
+    const user = await getUserById(req.params.id);
+
+    if(!user)
+      return res.status(404).json({message:"User not found"});
+
+    return res.status(200).json({data: user});
+   }catch{
+    return res.status(500).json({message: "Internal server error"});
+   }
+};
+
+// CREATE USER (POST)
+const registerUser = async (req, res) => {
+  const {username, email, password} = req.body;
+
+  if(!username || !email || !password){
+    return res.status(400).json({error: "All fields are required"});
+  }
+
+  try{
+    const user = await createUser(username, email, password);
+
+    return res.status(201).json({
+      message: "User created successfully",
+      userId: user.id,
+    });
+  }catch (error){
+    return res.status(500).json({error: error.message});
+  }
+};
+
+// DELETE USER BY ID
+const deleteUser = (req, res) => {
+  try{
+    const deleted = await removeUser(req.params.id);
+
+    if(deleted === 0)
+      return res.status(404).json({message: "User not found"});
+
+    return res.status(200).json({message: "User deleted successfully"});
+  }catch{
+    return res.status(500).json({message: "Internal server error"});
+  }
+};
+
 
 const handleLogin = async (req, res, next) => {
   if (!req.body.email || !req.body.password) {
@@ -99,21 +103,6 @@ const handleLogin = async (req, res, next) => {
   }
 };
 
-const deleteUser = (req, res, next) => {
-  db.query(
-    `DELETE FROM users WHERE id = $1`,
-    [req.params.id],
-    (err, result) => {
-      if (err) {
-        return res.status(500).json({ message: "Internal server error" });
-      }
-      if (result.affectedRows === 0) {
-        return res.status(404).json({ message: "User not found" });
-      }
 
-      return res.status(200).json({ message: "User deleted successfully" });
-    },
-  );
-};
 
 module.exports = { getUsers, getUser, registerUser, deleteUser, handleLogin };
